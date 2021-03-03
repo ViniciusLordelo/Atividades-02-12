74d2e8731201ea2268298f8109ce429dfc480e05
Vinicius Lordelo <73176059+ViniciusLordelo@users.noreply.github.com>
2021-03-03 19:06:41 -0300

    Atividade completa

---
+++ Ativ02.12/exerc.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using exerc._2.classe;
+namespace exerc._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            celular celular = new celular();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja ligar o celular? s/n");
+            string resposta = Console.ReadLine();
+            if (resposta == "s")
+            {
+               Console.WriteLine("Deseja usar qual modelo de celular?");
+               celular.modelo = Console.ReadLine();
+
+               Console.WriteLine("");
+            }
+            else{
+                Console.WriteLine("Você não pode executar nenhuma ação se seu celular estiver desligado!");
+            }
+        }
+    }
+}
---
+++ Ativ02.12/exerc.2/classe/celular.cs
@@ -0,0 +1,11 @@
+namespace exerc._2.classe
+{
+    public class celular
+    {
+        public string cor;
+        public string modelo;
+        public string tamanho;
+        public bool ligado;
+        
+    }
+}
